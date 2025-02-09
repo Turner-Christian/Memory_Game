e650b8d52b76ec0d60e45a88e0ebc227a4b3f3c9
Christian Turner <keycoderk@gmail.com>
2025-02-09 18:15:51 -0500

    2nd push

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import _ from 'lodash'
+import React, { onClick, useState, useEffect } from "react";
+import _, { set } from "lodash";
 
 const MemoryGame = ({ images }) => {
   const dupedImages = [...images, ...images];
-  let shuffledImages = _.shuffle(dupedImages)
+  const [imgClicked, setImgClicked] = useState([]);
+  const [shuffledImages, setShuffledImages] = useState([]);
 
-  console.log(dupedImages);
+  useEffect(() => {
+    setShuffledImages(_.shuffle(dupedImages));
+  }, []);
+
+
+  function matchingCards(src, key) {
+    if (src === imgClicked[0] && key != imgClicked[1]) {
+      console.log("MATCH!");
+    }
+    setImgClicked([src, key]);
+  }
+
   return (
-    <div className="body">
+    <div className="grid">
       {shuffledImages.map((image, index) => (
-          <img key={index} src={image} alt={`image index ${index}`}/>
+        <div className="card">
+          <img
+            onClick={() => matchingCards(image, index)}
+            key={index}
+            className="image"
+            src={image}
+            // alt={`image index ${index}`}
+          />
+        </div>
       ))}
     </div>
   );
