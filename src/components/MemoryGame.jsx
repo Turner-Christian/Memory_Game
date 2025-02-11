373bf6cdee58456a1eda16c82404cc14dfb58632
Christian Turner <keycoderk@gmail.com>
2025-02-11 13:49:33 -0500

    Added cover image for cards

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -5,6 +5,9 @@ const MemoryGame = ({ images }) => {
   const dupedImages = [...images, ...images];
   const [imgClicked, setImgClicked] = useState([]);
   const [shuffledImages, setShuffledImages] = useState([]);
+  const [imgFlipped, setImgFlipped] = useState()
+
+  const coveredCard = "https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
   useEffect(() => {
     setShuffledImages(_.shuffle(dupedImages));
@@ -26,7 +29,7 @@ const MemoryGame = ({ images }) => {
             onClick={() => (matchingCards(image, index), setImgFlipped(index))}
             key={index}
             className={"image"}
-            src={image}
+            src={index == imgFlipped ? image : coveredCard}
             alt={`image index ${index}`}
           />
         </div>
