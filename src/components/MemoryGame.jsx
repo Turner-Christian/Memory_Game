8f5adff42c74e9ea81bc47bcdebf85963028d13b
Christian Turner <keycoderk@gmail.com>
2025-02-14 13:51:16 -0500

    trying to redo factoring

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -5,12 +5,22 @@ const MemoryGame = ({ images }) => {
   const dupedImages = [...images, ...images];
   const [imgClicked, setImgClicked] = useState([]);
   const [shuffledImages, setShuffledImages] = useState([]);
-  const [imgFlipped, setImgFlipped] = useState()
 
   const coveredCard = "https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  const [groupedImgs, setGroupedImgs] = useState([])
+
+
 
   useEffect(() => {
     setShuffledImages(_.shuffle(dupedImages));
+    for(i = 0; i < (len(shuffledImages) / 2); i++){
+      if(groupedImgs.includes(shuffledImages[i])){
+        
+      } 
+      else{
+        groupedImgs.push([shuffledImages[i]])
+      }
+    }
   }, []);
 
 
@@ -26,10 +36,10 @@ const MemoryGame = ({ images }) => {
       {shuffledImages.map((image, index) => (
         <div className="card">
           <img
-            onClick={() => (matchingCards(image, index), setImgFlipped(index))}
+            onClick={() => (matchingCards(image, index))}
             key={index}
             className={"image"}
-            src={index == imgFlipped ? image : coveredCard}
+            src={image}
             alt={`image index ${index}`}
           />
         </div>
